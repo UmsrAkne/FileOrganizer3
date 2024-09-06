15311bd88df1376e2008f201f95fb47021a64552
Akn <sibata486@gmail.com>
2024-09-07 00:16:19 +0900

    add / テストプロジェクトを追加。

---
+++ FileOrganizer3Tests/Models/FileContainerTests.cs
@@ -0,0 +1,12 @@
+﻿namespace FileOrganizer3Tests.Models
+{
+    [TestFixture]
+    public class FileContainerTests
+    {
+        [Test]
+        public void ReIndexTest()
+        {
+            
+        }
+    }
+}
