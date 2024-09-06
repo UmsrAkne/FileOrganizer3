1d3d9296bbf4e8f7053948a7bf57b86deb2c7248
Akn <sibata486@gmail.com>
2024-09-06 20:58:57 +0900

    init

---
+++ FileOrganizer3/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,15 @@
+﻿using FileOrganizer3.Models;
+using Prism.Mvvm;
+
+namespace FileOrganizer3.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class MainWindowViewModel : BindableBase
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        public TextWrapper TextWrapper { get; private set; } = new ();
+    }
+}
---
+++ FileOrganizer3/Views/MainWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace FileOrganizer3.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
