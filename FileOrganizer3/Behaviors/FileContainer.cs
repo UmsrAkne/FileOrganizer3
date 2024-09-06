5f387d265ecb603e65f475db3e1d8c304c70ff2b
Akn <sibata486@gmail.com>
2024-09-07 00:12:05 +0900

    add feat / FileInfoWrappers の中身にインデックスを割り振る処理を追加。

--- FileOrganizer3/Behaviors/FileContainer.cs
+++ FileOrganizer3/Behaviors/FileContainer.cs
@@ -14,6 +14,17 @@ namespace FileOrganizer3.Behaviors
         {
             var fileInfos = filePaths.Select(p => new FileInfo(p));
             FileInfoWrappers.AddRange(fileInfos.Select(f => new FileInfoWrapper(f)));
+            ReIndex(FileInfoWrappers);
+        }
+
+        public void ReIndex(IEnumerable<FileInfoWrapper> files)
+        {
+            var idx = 1;
+
+            foreach (var f in files)
+            {
+                f.Index = idx++;
+            }
         }
     }
 }
