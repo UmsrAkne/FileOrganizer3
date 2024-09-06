b520acd94b10d548775ce3119b6346e1a939e496
Akn <sibata486@gmail.com>
2024-09-07 00:51:21 +0900

    add / デバッグ実行時、ダミーのデータが初期値に設定されるようにした。

---
+++ FileOrganizer3/Models/DummyFileProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace FileOrganizer3.Models
+{
+    public class DummyFileProvider
+    {
+        public static IEnumerable<FileInfoWrapper> GetDummyFiles()
+        {
+            return new List<FileInfoWrapper>()
+            {
+                new (new FileInfo("test_test_test111.mp3")) { Index = 1, },
+                new (new FileInfo("test_test_test222.mp3")) { Index = 2, },
+                new (new FileInfo("test_test_test333.mp3")) { Index = 3, },
+                new (new FileInfo("test_test_test444.mp3")) { Index = 99, },
+            };
+        }
+    }
+}
