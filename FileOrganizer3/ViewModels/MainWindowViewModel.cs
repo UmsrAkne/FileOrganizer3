060e84dc2007b5ceee631dfeb036d11f4eec10c6
Akn <sibata486@gmail.com>
2024-09-08 00:40:57 +0900

    add feat / キー操作によってサウンドファイルを再生可能にした。

--- FileOrganizer3/ViewModels/MainWindowViewModel.cs
+++ FileOrganizer3/ViewModels/MainWindowViewModel.cs
@@ -1,14 +1,18 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using FileOrganizer3.Behaviors;
 using FileOrganizer3.Models;
+using Prism.Commands;
 using Prism.Mvvm;
 
 namespace FileOrganizer3.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
-    public class MainWindowViewModel : BindableBase, IMainWindowViewModel
+    public class MainWindowViewModel : BindableBase, IMainWindowViewModel, IDisposable
     {
+        private readonly SoundPlayer soundPlayer = new ();
+
         public MainWindowViewModel()
         {
             SetDummyData();
@@ -17,6 +21,32 @@ namespace FileOrganizer3.ViewModels
         public TextWrapper TextWrapper { get; private set; } = new ();
 
         public FileContainer FileContainer { get; set; } = new ();
+
+        /// <summary>
+        /// FileContainer 上で選択されいてるアイテムがサウンドファイルかを確認した後、再生します。
+        /// PlayCommand は FileContainer に宣言するのが自然ですが、履歴ウィンドウの方にも情報を送る都合上ここに宣言しています。
+        /// </summary>
+        public DelegateCommand PlaySoundCommand => new DelegateCommand(() =>
+        {
+            var item = FileContainer.SelectedItem;
+            if (item is not { IsSoundFile: true, })
+            {
+                return;
+            }
+
+            soundPlayer.PlayAudio(item);
+        });
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            soundPlayer?.Dispose();
+        }
 
         [Conditional("DEBUG")]
         private void SetDummyData()
