58b20d1a9223b41e17d4a984c44274b1d8fb911c
Akn <sibata486@gmail.com>
2024-09-10 18:03:50 +0900

    add / FileInfoWrapper のリストからの抽出対象を表す enum を追加。

---
+++ FileOrganizer3/Models/ExtractOption.cs
@@ -0,0 +1,20 @@
+﻿namespace FileOrganizer3.Models
+{
+    public enum ExtractOption
+    {
+        /// <summary>
+        /// 全てのアイテムを対象にします。
+        /// </summary>
+        All,
+
+        /// <summary>
+        /// `IsIgnored` が `true` のアイテムを対象にします。
+        /// </summary>
+        Ignored,
+
+        /// <summary>
+        /// `IsMarked` が `true` のアイテムを対象にします。ただし、 `IsIgnored` が true のアイテムは対象外です。
+        /// </summary>
+        Marked,
+    }
+}
