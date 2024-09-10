f9993702c7ddb783f566209e4fa8d1c3af09750a
Akn <sibata486@gmail.com>
2024-09-10 18:45:25 +0900

    add feat / マークしたファイルにプレフィックス・サフィックスをつけるコマンドを実装。メニューバーに設置。

--- FileOrganizer3/Models/FileContainer.cs
+++ FileOrganizer3/Models/FileContainer.cs
@@ -2,6 +2,7 @@
 using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
+using System.Windows.Documents;
 using Prism.Commands;
 using Prism.Mvvm;
 
@@ -53,6 +54,35 @@ namespace FileOrganizer3.Models
             ReIndex(FileInfoWrappers);
         });
 
+        public DelegateCommand<RenameOption> AppendTextToNameCommand => new DelegateCommand<RenameOption>((option) =>
+        {
+            if (option == null)
+            {
+                return;
+            }
+
+            var files = ExtractFiles(FileInfoWrappers, option.ExtractOption).ToList();
+            foreach (var fb in files.Where(f => f.TemporaryName == string.Empty))
+            {
+                fb.TemporaryName = Path.GetFileNameWithoutExtension(fb.Name);
+            }
+
+            if (option.IsPrefix)
+            {
+                foreach (var f in files)
+                {
+                    f.TemporaryName = $"{option.Text}_{f.TemporaryName}";
+                }
+
+                return;
+            }
+
+            foreach (var f in files)
+            {
+                f.TemporaryName = $"{f.TemporaryName}_{option.Text}";
+            }
+        });
+
         public void AddFiles(IEnumerable<string> filePaths)
         {
             var fileInfos = filePaths.Select(p => new FileInfo(p));
@@ -67,6 +97,21 @@ namespace FileOrganizer3.Models
             foreach (var f in files)
             {
                 f.Index = f.IsIgnored ? 0 : idx++;
+            }
+        }
+
+        private IEnumerable<FileInfoWrapper> ExtractFiles(IEnumerable<FileInfoWrapper> files, ExtractOption option)
+        {
+            switch (option)
+            {
+                case ExtractOption.All:
+                    return files;
+                case ExtractOption.Marked:
+                    return files.Where(f => f.IsMarked && !f.IsIgnored);
+                case ExtractOption.Ignored:
+                    return files.Where(f => f.IsIgnored);
+                default:
+                    return files;
             }
         }
     }
