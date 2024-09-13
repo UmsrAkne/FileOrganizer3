386b621705348337ec2e919efb12d49a25de8099
Akn <sibata486@gmail.com>
2024-09-13 19:09:48 +0900

    add view / 再生履歴ウィンドウを折り畳めるようにした。

--- FileOrganizer3/Models/AppearanceManager.cs
+++ FileOrganizer3/Models/AppearanceManager.cs
@@ -7,16 +7,37 @@ namespace FileOrganizer3.Models
     public class AppearanceManager : BindableBase
     {
         private double fontSize = 13.0;
+        private GridLength historyPanelHeight = new GridLength(150.0);
+        private double savedHistoryPanelHeight;
+        private Visibility historyListVisibility;
 
-        public Visibility HistoryListVisibility { get; set; }
+        public Visibility HistoryListVisibility
+        {
+            get => historyListVisibility;
+            set => SetProperty(ref historyListVisibility, value);
+        }
 
         public double FontSize { get => fontSize; set => SetProperty(ref fontSize, value); }
 
+        public GridLength HistoryPanelHeight
+        {
+            get => historyPanelHeight;
+            set => SetProperty(ref historyPanelHeight, value);
+        }
+
         public DelegateCommand ToggleHistoryListVisibilityCommand => new DelegateCommand(() =>
         {
-            HistoryListVisibility = HistoryListVisibility == Visibility.Visible
-                ? Visibility.Collapsed
-                : Visibility.Visible;
+            if (HistoryPanelHeight.Value == 0)
+            {
+                HistoryPanelHeight = new GridLength(savedHistoryPanelHeight);
+                HistoryListVisibility = Visibility.Visible;
+            }
+            else
+            {
+                savedHistoryPanelHeight = HistoryPanelHeight.Value;
+                HistoryPanelHeight = new GridLength(0);
+                HistoryListVisibility = Visibility.Hidden;
+            }
         });
 
         public DelegateCommand<double?> SetFontSizeCommand => new DelegateCommand<double?>((param) =>
