463c1ae9cbc55cf8bedd0d6d90fd92a0be886c76
Akn <sibata486@gmail.com>
2024-09-14 22:07:25 +0900

    add file / ListBox のカーソル位置を管理するクラスを追加。

---
+++ FileOrganizer3/Models/CursorManager.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.ObjectModel;
+using Prism.Mvvm;
+
+namespace FileOrganizer3.Models
+{
+    public class CursorManager : BindableBase
+    {
+        private FileInfoWrapper selectedItem;
+        private int selectedIndex;
+
+        public ObservableCollection<FileInfoWrapper> Items { get; set; }
+
+        public FileInfoWrapper SelectedItem
+        {
+            get => selectedItem;
+            set => SetProperty(ref selectedItem, value);
+        }
+
+        public int SelectedIndex { get => selectedIndex; set => SetProperty(ref selectedIndex, value); }
+
+        public void MoveCursorUp()
+        {
+            SelectedIndex--;
+        }
+
+        public void MoveCursorDown()
+        {
+            SelectedIndex++;
+        }
+
+        public void MoveCursorToTop()
+        {
+            if (Items == null || Items.Count == 0)
+            {
+                return;
+            }
+
+            SelectedIndex = 0;
+        }
+
+        public void MoveCursorToBottom()
+        {
+            if (Items == null || Items.Count == 0)
+            {
+                return;
+            }
+
+            SelectedIndex = Items.Count - 1;
+        }
+    }
+}
