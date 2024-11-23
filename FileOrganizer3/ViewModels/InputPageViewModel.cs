2a785fd9f57cc3b36b54f32844a17912859175c7
Akn <sibata486@gmail.com>
2024-11-23 15:39:00 +0900

    add files / インデックスを入力するダイアログに関するファイルを追加。

---
+++ FileOrganizer3/ViewModels/InputPageViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+
+namespace FileOrganizer3.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class InputPageViewModel : BindableBase, IDialogAware
+    {
+        private string title = "Input page";
+
+        public event Action<IDialogResult> RequestClose;
+
+        public string Title { get => title; private set => SetProperty(ref title, value); }
+
+        public DelegateCommand CloseCommand => new DelegateCommand(() =>
+        {
+            RequestClose?.Invoke(new DialogResult());
+        });
+
+        public bool CanCloseDialog() => true;
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+    }
+}
---
+++ FileOrganizer3/Views/InputPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace FileOrganizer3.Views
+{
+    public partial class InputPage : Page
+    {
+        public InputPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
