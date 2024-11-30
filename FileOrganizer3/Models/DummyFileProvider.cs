2a2412f80a462fc7807ce7b616614cf6626dfc47
Akn <sibata486@gmail.com>
2024-11-30 14:05:14 +0900

    add / デバッグ起動時、大量のダミーファイルを注入。

--- FileOrganizer3/Models/DummyFileProvider.cs
+++ FileOrganizer3/Models/DummyFileProvider.cs
@@ -7,7 +7,7 @@ namespace FileOrganizer3.Models
     {
         public static IEnumerable<FileInfoWrapper> GetDummyFiles()
         {
-            return new List<FileInfoWrapper>()
+            var fileWrappers = new List<FileInfoWrapper>()
             {
                 new (new FileInfo("test_test_test111.mp3")) { Index = 1, },
                 new (new FileInfo("test_test_test222.mp3")) { Index = 2, IsIgnored = true, },
@@ -15,11 +15,17 @@ namespace FileOrganizer3.Models
                 new (new FileInfo("test_test_test444.mp3")) { Index = 4, IsMarked = true, IsIgnored = true, },
                 new (new FileInfo("test_test_test555.mp3")) { Index = 5, Playing = true, },
                 new (new FileInfo("test_test_test666.mp3")) { Index = 6, },
-                new (new FileInfo("test_test_test444.mp3")) { Index = 99, },
-                new (new FileInfo("testSounds\\se1.mp3")) { Index = 100, },
-                new (new FileInfo("testSounds\\se2.ogg")) { Index = 101, },
-                new (new FileInfo("testSounds\\se3.wav")) { Index = 102, },
             };
+
+            for (var i = 0; i < 2000; i++)
+            {
+                fileWrappers.Add(new FileInfoWrapper(new FileInfo($"dummyFile{i + 7:D5}.mp3"))
+                {
+                    Index = i + 7,
+                });
+            }
+
+            return fileWrappers;
         }
     }
 }
