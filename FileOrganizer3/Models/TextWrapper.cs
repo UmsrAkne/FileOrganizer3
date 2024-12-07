46b4f6288a6602b45a3f8d38dda1444aae5c2e7b
Akn <sibata486@gmail.com>
2024-12-07 16:22:05 +0900

    Update version number.

--- FileOrganizer3/Models/TextWrapper.cs
+++ FileOrganizer3/Models/TextWrapper.cs
@@ -34,7 +34,7 @@ namespace FileOrganizer3.Models
         [Conditional("RELEASE")]
         private void SetVersion()
         {
-            Version = "20241130" + "a";
+            Version = "20241207" + "a";
         }
 
         [Conditional("DEBUG")]
