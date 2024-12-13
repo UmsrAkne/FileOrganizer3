3907e3c7301b69c2ae2d184145a2353dc85ddacd
Akn <sibata486@gmail.com>
2024-12-13 18:10:35 +0900

    add prop / FileCopyPage で必要になるプロパティをビューモデルに追加。UIとバインディングした。

--- FileOrganizer3/ViewModels/FileCopyPageViewModel.cs
+++ FileOrganizer3/ViewModels/FileCopyPageViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using FileOrganizer3.Models;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
@@ -8,13 +10,33 @@ namespace FileOrganizer3.ViewModels
     // ReSharper disable once ClassNeverInstantiated.Global
     public class FileCopyPageViewModel : BindableBase, IDialogAware
     {
+        private string fileListText;
+        private string logText;
+        private string fileDestinationPath;
+
         public event Action<IDialogResult> RequestClose;
 
         public string Title => "File Copy Page";
 
+        public string FileListText { get => fileListText; set => SetProperty(ref fileListText, value); }
+
+        public string LogText { get => logText; set => SetProperty(ref logText, value); }
+
+        public List<FileInfoWrapper> FileInfoWrappers { get; set; }
+
+        public string FileDestinationPath
+        {
+            get => fileDestinationPath;
+            set => SetProperty(ref fileDestinationPath, value);
+        }
+
         public DelegateCommand CloseCommand => new DelegateCommand(() =>
         {
             RequestClose?.Invoke(new DialogResult());
+        });
+
+        public DelegateCommand CopyFilesCommand => new DelegateCommand(() =>
+        {
         });
 
         public bool CanCloseDialog() => true;
