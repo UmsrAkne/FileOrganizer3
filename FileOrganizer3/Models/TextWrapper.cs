03bffc9d3c8171fd5aeae9440495f10871c900ad
UmsrAkne <sibata486@gmail.com>
2025-04-30 15:45:26 +0900

    update version number.

--- FileOrganizer3/Models/TextWrapper.cs
+++ FileOrganizer3/Models/TextWrapper.cs
@@ -34,7 +34,7 @@ namespace FileOrganizer3.Models
         [Conditional("RELEASE")]
         private void SetVersion()
         {
-            Version = "20241218" + "a";
+            Version = "20250430" + "a";
         }
 
         [Conditional("DEBUG")]
